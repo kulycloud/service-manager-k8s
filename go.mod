b897241bd338f2103c958eeb61918fc66d8e0003
lucaschimweg <luca@schimweg.net>
2021-03-23 12:26:40 +0100

    KU-109 Add periodic reconcile to ReconcileScheduler

--- go.mod
+++ go.mod
@@ -3,8 +3,8 @@ module github.com/kulycloud/service-manager-k8s
 go 1.15
 
 require (
-	github.com/kulycloud/common v0.0.0-20210319003959-530ee5e8b1e3
-	github.com/kulycloud/protocol v0.0.0-20210319003638-857838179fbd
+	github.com/kulycloud/common v0.0.0-20210323100819-93d825d597b5
+	github.com/kulycloud/protocol v0.0.0-20210323100304-4caa455444f5
 	k8s.io/api v0.19.0
 	k8s.io/apimachinery v0.19.0
 	k8s.io/client-go v0.19.0
--- reconciling/scheduling.go
+++ reconciling/scheduling.go
@@ -3,19 +3,31 @@ package reconciling
 import (
 	"context"
 	commonCommunication "github.com/kulycloud/common/communication"
+	"time"
 )
 
 const ResourceTypeService = "service"
+const ReconcilePeriod = 1 * time.Hour
+const ReconcileCheckLoop = 5 * time.Minute
+const ReconcileLoopErrorRetry = 1 * time.Minute
+const TriggerPeriod = "period"
+const TriggerEvent = "event"
 
 type ReconcileScheduler struct {
-	Reconciler Reconciler
-	storage    *commonCommunication.StorageCommunicator
+	Reconciler      Reconciler
+	storage         *commonCommunication.StorageCommunicator
+	namespaces      map[string]time.Time
+	stop            bool
+	storageNotifier chan interface{}
 }
 
 func NewReconcilerScheduler(storage *commonCommunication.StorageCommunicator, reconciler Reconciler) (*ReconcileScheduler, error) {
 	return &ReconcileScheduler{
 		Reconciler: reconciler,
-		storage: storage,
+		storage:    storage,
+		stop:       false,
+		namespaces: make(map[string]time.Time),
+		storageNotifier: make(chan interface{}),
 	}, nil
 }
 
@@ -41,34 +53,101 @@ func (scheduler *ReconcileScheduler) onConfigurationChangedEvent(event *commonCo
 		return
 	}
 
-	logger.Infow("reconciling namespace",
-		"trigger", "event",
-		"namespace", event.Resource.Namespace)
+	scheduler.ReconcileNamespace(context.Background(), event.Resource.Namespace, TriggerEvent)
+}
 
-	err := scheduler.Reconciler.ReconcileDeployments(context.Background(), event.Resource.Namespace)
-	if err != nil {
+func (scheduler *ReconcileScheduler) onStorageChangedEvent(event *commonCommunication.StorageChanged) {
+	if scheduler.storageNotifier != nil {
+		go func() {
+			select { // clear stream
+				case <-scheduler.storageNotifier:
+				default:
+			}
+
+			if scheduler.storage.Ready() {
+				 scheduler.storageNotifier <- nil
+			}
+		}()
+	}
+
+	scheduler.Reconciler.PropagateStorageToLoadBalancers(context.Background(), event.Endpoints)
+}
+
+func (scheduler *ReconcileScheduler) ReconcileNamespace(ctx context.Context, namespace string, trigger string) {
+	logger.Infow("reconciling namespace",
+		"trigger", trigger,
+		"namespace", namespace)
+	err := scheduler.Reconciler.ReconcileDeployments(ctx, namespace)
+	if err == nil {
+		scheduler.namespaces[namespace] = time.Now()
+	} else {
 		logger.Errorw("error reconciling namespace",
-			"trigger", "event",
-			"namespace", event.Resource.Namespace,
+			"trigger", "period",
+			"namespace", namespace,
 			"error", err)
 	}
 }
 
-func (scheduler *ReconcileScheduler) onStorageChangedEvent(event *commonCommunication.StorageChanged) {
-	scheduler.Reconciler.PropagateStorageToLoadBalancers(context.Background(), event.Endpoints)
+func (scheduler *ReconcileScheduler) needsReconcile(namespace string) bool {
+	t, ok := scheduler.namespaces[namespace]
+	return !ok || time.Now().Sub(t) >= ReconcilePeriod
+}
+
+func (scheduler *ReconcileScheduler) checkNamespaces(ctx context.Context) error {
+	logger.Infow("reconciling namespaces",
+		"trigger", TriggerPeriod)
+	namespaces, err := scheduler.storage.GetNamespaces(ctx)
+	if err != nil {
+		return err
+	}
+
+	for _, namespace := range namespaces {
+		if scheduler.needsReconcile(namespace) {
+			scheduler.ReconcileNamespace(ctx, namespace, TriggerPeriod)
+		}
+	}
+	return nil
+}
+
+func (scheduler *ReconcileScheduler) reconcileLoop() {
+	ctx := context.Background()
+
+	for !scheduler.stop {
+		if !scheduler.storage.Ready() {
+			logger.Warnw("trying to reconcile but storage is not ready")
+			time.Sleep(ReconcileLoopErrorRetry)
+			continue
+		}
+		err := scheduler.checkNamespaces(ctx)
+		if err == nil {
+			time.Sleep(ReconcileCheckLoop)
+		} else {
+			time.Sleep(ReconcileLoopErrorRetry)
+		}
+	}
 }
 
 func (scheduler *ReconcileScheduler) Start() <-chan error {
 	errStream := make(chan error)
 
 	go func() {
-		err := scheduler.Reconciler.MonitorCluster(context.Background())
-		if err != nil {
-			errStream <- err
-		}
-	}()
+		// wait for storage to become available
+		<- scheduler.storageNotifier
+		scheduler.storageNotifier = nil
 
-	// Periodically check for new namespaces and reconcile!
+		for !scheduler.storage.Ready() {
+			time.Sleep(10 * time.Second)
+		}
+
+		go func() {
+			err := scheduler.Reconciler.MonitorCluster(context.Background())
+			if err != nil {
+				errStream <- err
+			}
+		}()
+
+		scheduler.reconcileLoop()
+	}()
 
 	return errStream
 }
